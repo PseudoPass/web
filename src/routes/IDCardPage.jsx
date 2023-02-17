94a070d856438419ddee5a2527deba18e0402974
Michael Ruiz <michael@mruiz.dev>
2023-02-17 07:30:07 -0800

    Added ID Card Page

---
+++ src/routes/IDCardPage.jsx
@@ -0,0 +1,13 @@
+import NavigationBar from "../components/NavigationBar/NavigationBar";
+import IdentificationCard from "../components/IdentificationCard/IdentificationCard";
+import './styles/IDCardPage.css';
+function IDCardPage() {
+    return(
+        <div>
+            <NavigationBar />
+            <IdentificationCard />
+        </div>
+    );
+}
+
+export default IDCardPage;
---
+++ src/routes/SettingsPage.jsx
@@ -0,0 +1,12 @@
+import NavigationBar from "../components/NavigationBar/NavigationBar";
+
+function SettingsPage() {
+    return(
+    <div>
+        <NavigationBar />
+
+    </div>
+    );
+}
+
+export default SettingsPage;
