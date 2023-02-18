aa449904ddb0f226ba5fce8703f093893855c0a2
Michael Ruiz <michael@mruiz.dev>
2023-02-17 23:15:46 -0800

    Make login button look cleaner, fix login state on load

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,10 +1,12 @@
+import "./styles/HomePage.css";
 import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
 function Homepage() {
-    const [ user, setUser ] = useState([]);
-    const [ profile, setProfile ] = useState([]);
+    const [ user, setUser ] = useState(null);
+    const [ profile, setProfile ] = useState(null);
 
     const login = useGoogleLogin({
         onSuccess: (codeResponse) => setUser(codeResponse),
@@ -37,10 +39,9 @@ function Homepage() {
     };
 
     return (
-        <div>
-            <h2>React Google Login</h2>
-            <br />
-            <br />
+        <div className={"main-container"}>
+            <div className={"logo-text"}>Pseudo Pass</div>
+            <div className={"login-text"}>Login using SJSU Credentials</div>
             {profile ? (
                 <div>
                     <img src={profile.picture} alt="user image" />
@@ -52,7 +53,27 @@ function Homepage() {
                     <button onClick={logOut}>Log out</button>
                 </div>
             ) : (
-                <button onClick={() => login()}>Sign in with Google 🚀 </button>
+
+                <Button
+                    className={"login-button"}
+                    variant="outline-dark"
+                    onClick={() => login()} >
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"
+                         className="google-icon">
+                        <g>
+                            <path className="google-icon-red" fill="#EA4335"
+                                  d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                            <path className="google-icon-blue" fill="#4285F4"
+                                  d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                            <path className="google-icon-yellow" fill="#FBBC05"
+                                  d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                            <path className="google-icon-green" fill="#34A853"
+                                  d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                            <path fill="none" d="M0 0h48v48H0z"></path>
+                        </g>
+                    </svg>
+                    Continue with Google
+                </Button>
             )}
         </div>
     );
