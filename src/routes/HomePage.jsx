bcaf8990731c13c493965fd4709b0ff5e897ccea
moonnada <kimbm2014@gmail.com>
2023-02-26 18:45:01 -0800

    apply media queries

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
-import { Carousel, CarouselItem } from 'react-bootstrap';
 import './styles/HomePage.scss';
 import Animation from '../components/Animation/Animation';
-import { Breadcrumb, Layout, Typography, theme } from 'antd';
+import { Layout, Typography } from 'antd';
 import Logo from './img/logo.png';
 import FirstImg from './img/firstImg.png';
 import GoogleLoginButton from '../components/Login/GoogleLoginButton';
@@ -22,11 +21,11 @@ const HomePage = (props) => {
         </Header>
         <Content>
           <div className="content">
-            <div className="green-site-layout-content">
+            <div className="first-layout-content">
               <div className="left_content">
                 <img src={FirstImg} className="firstImg" />
               </div>
-              <div className="right_content">
+              <div className="fist_right_content">
                 <Title level={2}>
                   PseudoPass believes there is a better way
                 </Title>
@@ -36,13 +35,24 @@ const HomePage = (props) => {
                   <br></br> Can you really trust yourself, if you aren’t
                   PseudoPassed?
                 </Paragraph>
-                <div className={'login-text'}>
-                  Login using <b>SJSU</b> Credentials
+
+                <div className="loginPart">
+                  <Paragraph
+                    style={{
+                      fontSize: 17,
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    Login using&nbsp;<b>SJSU</b>&nbsp;Credentials
+                  </Paragraph>
                 </div>
-                <GoogleLoginButton setProfile={setProfile} />
+                <div className="googleBtn">
+                  <GoogleLoginButton setProfile={setProfile} />
+                </div>
               </div>
             </div>
-            <div className="blue-site-layout-content">
+            <div className="layout-content" style={{ background: '#defcf9' }}>
               <div className="left_content">
                 <Title level={2}>Pseudo Pass 101</Title>
                 <Paragraph style={{ fontSize: 17 }}>
@@ -62,26 +72,27 @@ const HomePage = (props) => {
                 />
               </div>
             </div>
-            <div
-              className="green-site-layout-content"
-              style={{ minHeight: '28rem' }}
-            >
-              <div className="left_content" style={{ paddingRight: '6rem' }}>
-                <iframe
+            <div className="layout-content" style={{ background: '#dfffd8' }}>
+              <div className="right_content">
+              <iframe
                   style={{ border: 'none' }}
                   title="Finger Print"
                   src="https://rive.app/s/LXIEKoNUAkO9hNuPmrPsdA/embed"
                   allowFullScreen
                 />
               </div>
-              <div className="right_content" style={{ paddingRight: '20rem' }}>
-                <Title level={2}>DIDs</Title>
+              <div className="left_content">
+                <Title level={2}>Pseudo Pass 101</Title>
                 <Paragraph style={{ fontSize: 17 }}>
-                  The quick brown fox jumps over the lazy dog.
+                  The quick brown fox jumps over the lazy dog. Lorem ipsum dolor
+                  sit amet, consectetur adipiscing elit. Fusce viverra, ligula a
+                  efficitur vestibulum, ipsum tellus bibendum lorem, quis
+                  euismod enim ante vel nibh. Suspendisse potenti. Sed eget
+                  mauris blandit, malesuada eros et, dignissim
                 </Paragraph>
               </div>
             </div>
-            <div className="blue-site-layout-content">
+            <div className="layout-content" style={{ background: '#defcf9' }}>
               <div className="left_content">
                 <Title level={2}>Pseudo Pass 101</Title>
                 <Paragraph style={{ fontSize: 17 }}>
@@ -107,51 +118,6 @@ const HomePage = (props) => {
           @ copyright 2023 by Pseudo Pass
         </Footer>
       </Layout>
-
-      {/* <div className="main-container">
-        <div className="logo-text">Pseudo Pass</div>
-        <div className="catchphrase">
-          PseudoPass believes there is a better way
-        </div>
-        <div className="carousel">
-          <Carousel>
-            <CarouselItem>
-              You don’t know you… until you pseudo pass through.
-            </CarouselItem>
-            <CarouselItem>
-              Your identity is eternal, Pseudo Pass it on the blockchain.
-            </CarouselItem>
-            <CarouselItem>
-              Can you really trust yourself, if you aren’t PseudoPassed?
-            </CarouselItem>
-          </Carousel>
-        </div>
-
-        <iframe
-          style={{ border: 'none' }}
-          title="pseudo pass"
-          width="540"
-          height="540"
-          src="https://rive.app/s/UCwCVZW5mECdeVn6dWKQoQ/embed"
-          allowFullScreen
-        />
-        <iframe
-          style={{ border: 'none' }}
-          title="Finger Print"
-          width="500"
-          height="500"
-          src="https://rive.app/s/LXIEKoNUAkO9hNuPmrPsdA/embed"
-          allowFullScreen
-        />
-        <iframe
-          style={{ border: 'none' }}
-          title="server"
-          width="700"
-          height="700"
-          src="https://rive.app/s/dlQ_86dYH06ZyXgR9BSd_g/embed"
-          allowFullScreen
-        />
-      </div> */}
     </>
   );
 };
